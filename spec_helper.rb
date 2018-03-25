e6c1cad6b362e7cd4916eb0a679b6d32f694d81e
Anastasia Chicu <anastasia.chicu@xing.com>
2018-03-25 16:21:04 +0200

    Use headless chrome

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'capybara'
 require 'capybara/rspec'
 require 'selenium-webdriver'
 require 'site_prism'
-Capybara.default_driver = :selenium
 
 require_relative './page_objects/job_page.rb'
 require_relative './page_objects/apply_page.rb'
@@ -10,3 +11,19 @@ require_relative './page_objects/jobs_list_page.rb'
 require_relative './page_objects/jobs_overview_page.rb'
 require_relative './page_objects/landing_page.rb'
 require_relative './page_objects/apply_page.rb'
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu] }
+  )
+
+  Capybara::Selenium::Driver.new app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
