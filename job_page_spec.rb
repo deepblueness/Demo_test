02c6aaef5efd9ef918a9aa927d0e0e18f4846e55
Anastasia Chicu <anastasia.chicu@xing.com>
2018-03-25 14:45:29 +0200

    Extract job_uuid to a variable

--- job_page_spec.rb
+++ job_page_spec.rb
@@ -4,35 +4,35 @@ require_relative './page_objects/job_page.rb'
 
 describe 'A job posting', type: :feature, js: true do
   let(:page) { SitePrism::Page::JobPage.new }
+  let(:job_uuid) { '65e7cf41-2b3b-48b4-a39e-0c4c04f29c1d' }
 
   it 'displays the job title' do
-    page.load
+    page.load(job_uuid: job_uuid)
     page.wait_for_job_title
     expect(page.job_title).to have_content('Lead QA Engineer (m/f)')
   end
   it 'displays the job location' do
-    page.load
+    page.load(job_uuid: job_uuid)
     page.wait_for_job_location
     expect(page.job_location).to have_content('Munich')
   end
   it 'displays the job description' do
-    page.load
-    page.wait_for_job_description
+    page.load(job_uuid: job_uuid)
     expect(page.job_description).to be_visible
     expect(page.job_description).to have_content('Are you passionate about building QA team')
   end
   it 'displays the your responsibilities section' do
-    page.load
+    page.load(job_uuid: job_uuid)
     expect(page.your_responsibilities).to be_visible
     expect(page.your_responsibilities_element.count).to eq(8)
   end
   it 'displays the your profile section' do
-    page.load
+    page.load(job_uuid: job_uuid)
     expect(page.your_profile).to be_visible
     expect(page.your_profile_element.count).to eq(7)
   end
   it 'displays the apply button' do
-    page.load
+    page.load(job_uuid: job_uuid)
     page.wait_for_apply_button
     expect(page).to have_apply_button
   end
--- page_objects/job_page.rb
+++ page_objects/job_page.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require_relative '../spec_helper'
 class JobPage < SitePrism::Page
-  set_url 'http://www.freeletics.com/de/corporate/jobs/65e7cf41-2b3b-48b4-a39e-0c4c04f29c1d'
+  set_url 'http://www.freeletics.com/de/corporate/jobs/{job_uuid}'
   element :job_title, '[itemprop="title"]'
   element :job_location, '[itemprop="jobLocation"]'
   element :job_description, '[itemprop="description"]'
